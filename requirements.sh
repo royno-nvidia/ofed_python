16dab645b38966195d74a06ebdddfbc619b0a9e4
Roy Novich <royno@nvidia.com>
2021-11-30 11:53:27 +0200

    requirements: Add colorlog installation
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,7 @@
 
 # this script will install modules required for scripts run
 
-sudo /usr/bin/python3.6 -m  pip install pydriller
-sudo /usr/bin/python3.6 -m  pip install pandas
-sudo /usr/bin/python3.6 -m  pip install xlsxwriter
+sudo /usr/bin/python3.7 -m  pip install pydriller
+sudo /usr/bin/python3.7 -m  pip install pandas
+sudo /usr/bin/python3.7 -m  pip install xlsxwriter
+sudo /usr/bin/python3.7 -m  pip install colorlog
