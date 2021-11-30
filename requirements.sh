50db0474ede8bd26b677de1e81b21432ea1b2f43
Roy Novich <royno@nvidia.com>
2021-11-30 13:41:57 +0200

    requirements: Search for available python interpater
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # this script will install modules required for scripts run
+sys_py_ver="$(ls  /usr/bin/  | grep -oE "python[0-9]\.*[0-9]*" | sort -r | uniq |head -1)"
 
-sudo /usr/bin/python3.7 -m  pip install pydriller
-sudo /usr/bin/python3.7 -m  pip install pandas
-sudo /usr/bin/python3.7 -m  pip install xlsxwriter
-sudo /usr/bin/python3.7 -m  pip install colorlog
+sudo /usr/bin/${sys_py_ver} -m  pip install pydriller
+sudo /usr/bin/${sys_py_ver} -m  pip install pandas
+sudo /usr/bin/${sys_py_ver} -m  pip install xlsxwriter
+sudo /usr/bin/${sys_py_ver} -m  pip install colorlog
+
+echo "---------------------------------------------"
+echo "Dependency modules install over ${sys_py_ver}"
+echo "---------------------------------------------"
