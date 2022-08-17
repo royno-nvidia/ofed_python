0af0c2cd3117c08c17b69c181953251c77d179df
Roy Novich <royno@nvidia.com>
2022-08-17 15:13:45 +0300

    backport_parser: Handle multiline ifdefs
    Signed-off-by: Roy Novich <royno@nvidia.com>

--- scripts/backport_parser.py
+++ scripts/backport_parser.py
@@ -13,10 +13,12 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Automatic full file '
                                         'review for defines and functions order')  
     # Add Arguments
-    parser.add_argument('-p', '--path', type=str, default=False,
+    parser.add_argument('--review', type=str, default=False,
                         help='Set a file path to review')
+    parser.add_argument('--reference', type=str, default=False,
+                        help='Set a file path to use as reference')
     parser.add_argument('-l', '--with_line_number', action='store_true',
-                        help='Set a line number')
+                        help='Get output with line number')
 
     # Parse the argument
     options = parser.parse_args()
@@ -36,9 +38,15 @@ def file_parser(args):
     tab = 0
 
     with open('output.csv', 'w') as output_file:
-        with open(args.path, 'r') as Read_file:
+        with open(args.review, 'r') as Read_file:
             # reading loop from a programing file
+            force_take_line = False
             for i, line in enumerate(Read_file.readlines()):
+                end_with_backslash = line.endswith('\\\n')
+                if force_take_line:
+                    debug_print(tab, line)
+                    write_into_file(args.with_line_number, i, tab, line, output_file)
+                    force_take_line = False
 
                 # searching for the regular expression that we need
                 if_pattern = re.search(f"\s*{if_statements}.*", line)
@@ -53,18 +61,21 @@ def file_parser(args):
                     debug_print(tab, if_pattern.group())
                     write_into_file(args.with_line_number, i, tab, if_pattern.group(), output_file)
                     tab += TAB_SIZE
+                    force_take_line = end_with_backslash
 
                 elif else_pattern:  # have else_statements
                     tab -= TAB_SIZE
                     debug_print(tab, else_pattern.group())
                     write_into_file(args.with_line_number, i, tab, else_pattern.group(), output_file)
                     tab += TAB_SIZE
+                    force_take_line = end_with_backslash
 
                 elif elif_pattern:  # have elif_statements
                     tab -= TAB_SIZE
                     debug_print(tab, elif_pattern.group())
                     write_into_file(args.with_line_number, i, tab, elif_pattern.group(), output_file)
                     tab += TAB_SIZE
+                    force_take_line = end_with_backslash
 
                 elif endif_pattern:  # have endif_statements
                     tab -= TAB_SIZE
